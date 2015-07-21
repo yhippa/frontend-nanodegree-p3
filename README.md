8be15b6ef968d0693e26543c60bd2d46b0aadf8e
Richard Yhip <richard.yhip@gmail.com>
2015-07-20 22:28:31 -0400

    Fixed formatting, style, and syntax issues. Added documentation to README.md

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 frontend-nanodegree-arcade-game
 ===============================
 
-Students should use this rubric: https://www.udacity.com/course/viewer#!/c-ud015/l-3072058665/m-3072588797
+Playing the game
+----------------
+To play the game you can:
+1.  Download this GitHub repository and open up the index.html file located at the root in any modern web browser
+2.  Go to http://yhippa.github.io/frontend-nanodegree-p3/ in any modern web browser
 
-for self-checking their submission.
+How to play
+-----------
+Use the arrow keys on the keyboard to move the avatar (player) around on any valid playing tile on the screen.  Avoid the bugs!  If you touch a bug you will go back to the beginning.  Once you cross the path and get to the river you have beaten the game and the game is over.  You go back to the beginning and can play again.
+
+Future features
+---------------
+* Scorekeeping
+* Three different difficulty levels
+* Powerups
+* Score boosters
+* Character selection
+
+Thanks for playing!  If you have any feedback please send it to me at richard.yhip@gmail.com.
--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-}
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -15,72 +15,80 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     this.x = this.x + this.speed * dt;
-}
+};
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+    // diagnostic code below to outline hitboxes
+    // ctx.rect(this.getLeftX(),this.getUpperY(),this.getRightX()-this.getLeftX(),this.getLowerY()-this.getUpperY());
+    // ctx.stroke(); 
+};
 
+// getLeftX() and getRightX() get the leftmost or rightmost hitbox on the X axis
+// getUpperY() and getLowerY() get the topmost or bottommost hitbox on the Y axis
 Enemy.prototype.getLeftX = function() {
     return this.x + 1;
-}
+};
 
 Enemy.prototype.getRightX = function() {
     return this.x + 1 + 99;
-}
+};
 
 Enemy.prototype.getUpperY = function() {
     return this.y + 71;
-}
+};
 
 Enemy.prototype.getLowerY = function() {
     return this.y + 71 + 77;
-}
+};
 
+// Creates a new random speed between 20 and 150 and creates a random row for the enemy
+var rowStartingPixels = [63, 146, 228];
 Enemy.prototype.reset = function() { 
     this.speed = (Math.random() * 150) + 20;
-    this.x = 0;
-    this.y = Math.floor((Math.random() * 3) + 1) * 70;
-}
+    this.x = -101;
+    this.y =  rowStartingPixels[Math.floor((Math.random() * 2) + 0)];
+};
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function() {
     this.sprite = 'images/char-boy.png';
-}
+};
 
-Player.prototype.update = function(dt) {
+Player.prototype.update = function() {
     this.x = this.x;
-}
+};
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    //originial sprites are 101x171
-}
+    // originial sprites are 101x171
+};
 
 Player.prototype.getLeftX = function() {
     return this.x + 1;
-}
+};
 
 Player.prototype.getRightX = function() {
     return this.x + 1 + 99;
-}
+};
 
 Player.prototype.getUpperY = function() {
     return this.y + 88;
-}
+};
 
 Player.prototype.getLowerY = function() {
     return this.y + 88 + 75;
-}
+};
 
 Player.prototype.reset = function() {
     this.x = this.xInit;
     this.y = this.yInit;
-}
+};
 
+// Takes a string direction and updates the player position if it is within the bounds of the game
 Player.prototype.handleInput = function(direction) {
     if (direction == 'left') {
         var nextX = this.x - 101;
@@ -102,15 +110,16 @@ Player.prototype.handleInput = function(direction) {
         if (isObjectOutOfBounds(this.x, nextY)) return;
         this.y = this.y + 83;
     }
-}
+};
 
+// Helper function to determine if a given object is out of bounds
 var isObjectOutOfBounds = function(xPos, yPos) {
     if (xPos < 0) return true;
     if (yPos < -50) return true;
     if (xPos > 405) return true;
     if (yPos > 400) return true;
     return false;
-} 
+};
 
 
 // Now instantiate your objects.
@@ -118,20 +127,16 @@ var isObjectOutOfBounds = function(xPos, yPos) {
 // Place the player object in a variable called player
 // enemies need to be -20 y to align with the stones
 var enemy1 = new Enemy();
-//enemy1.speed = 0;
 enemy1.reset();
 var enemy2 = new Enemy();
-enemy2.speed = 0;
 enemy2.reset();
 var enemy3 = new Enemy();
-enemy3.speed = 0;
 enemy3.reset();
 var allEnemies = [enemy1, enemy2, enemy3];
 var player = new Player();
 player.xInit = 202;
 player.yInit = 380;
-player.x = 202;
-player.y = 380;
+player.reset();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
